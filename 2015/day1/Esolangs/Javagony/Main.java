b7ec890f4bee28045d4a909a6f5484f1862a1160
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2020-12-12 22:56:51 +0100

    Added javagony solution to 2015 day1

---
+++ 2015/day1/Esolangs/Javagony/Main.java
@@ -0,0 +1,39 @@
+/**
+ * Javagony is an OOP programming language derived from Java. The only differences to Java are that following statements are not available in Javagony:
+ *
+ * for (){}
+ * if (){} //including else and else if
+ * while (){}
+ * do {} while ();
+ * switch(){}
+ * ?:
+ * 
+ * For more information check it's wiki page https://esolangs.org/wiki/Javagony
+ */
+public class Main {
+
+    private static int floor = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Processing: " + args[0]);
+        try {
+            reverseString(args[0]);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println(floor);
+        }
+        
+    }
+
+    public static String reverseString(String myStr)
+	{
+        int a = myStr.charAt(0);
+        try {
+            int b = floor/(a-40);
+        } catch (ArithmeticException e) {
+            ++floor;
+            ++floor;
+        }
+        --floor;
+		return reverseString(myStr.substring(1)) + myStr.charAt(0);
+	}
+}
