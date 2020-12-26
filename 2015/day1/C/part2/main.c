b114ac4d7084925e148ee7230b112ce9bc17078d
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2020-12-26 13:34:35 +0100

    Added C solution for 2015 day1 part2

---
+++ 2015/day1/C/part2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int
+main (int argc, char* argv[])
+{
+    printf("Processing: %s \n", argv[1]);
+    int floor = 0;
+    int i;
+    for (i = 0; i < strlen(argv[1]) && floor !=-1; ++i)
+    {
+        if(argv[1][i] == '(')
+        {
+            floor++;
+        } else {
+            floor--;
+        }
+    }
+    printf("%d \n", i);
+    return 0;
+}
